ca813af1b72fe68fed23c43bad11cfe05454df13
Nour Adel <noura7305@gmail.com>
2019-11-11 17:23:55 +0200

    streaming configration

--- client/src/components/streams/StreamShow.js
+++ client/src/components/streams/StreamShow.js
@@ -11,6 +11,27 @@ class StreamShow extends Component {
   }
   componentDidMount() {
     this.props.fetchStream(this.props.match.params.id);
+    this.buildPlayer();
+  }
+
+  componentDidUpdate() {
+    this.buildPlayer();
+  }
+
+  componentWillUnmount() {
+    this.player.destroy();
+  }
+
+  buildPlayer() {
+    if (!this.player || !this.props.stream) {
+      return;
+    }
+    this.player = flv.createPlayer({
+      type: "flv",
+      url: `http://localhost:8000/live/${this.props.match.params.id}.flv`
+    });
+    this.player.attachMediaElement(this.videoRef.current);
+    this.player.load();
   }
   render() {
     if (!this.props.stream) {
@@ -18,10 +39,7 @@ class StreamShow extends Component {
     }
     return (
       <div>
-        <video ef={this.videoRef}
-        style={{width:'100%'}}
-        controls
-        />
+        <video ef={this.videoRef} style={{ width: "100%" }} controls />
         <h1>{this.props.stream.title}</h1>
         <h5>{this.props.stream.description}</h5>
       </div>
