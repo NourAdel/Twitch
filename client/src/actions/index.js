c8c57c4ffc433603c6901b0c94de65822ed949f4
Nour Adel <noura7305@gmail.com>
2019-11-11 12:08:41 +0200

    programmatic navigation

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -8,6 +8,7 @@ import {
   EDIT_STREAM
 } from "./types";
 import streams from "../apis/streams";
+import history from '../history'
 export const signIn = userId => {
   return {
     type: SIGN_IN,
@@ -26,6 +27,9 @@ export const createStream = formValues => async(dispatch, getState) => {
   const response = await streams.post("/streams", {...formValues, userId});
 
   dispatch({ type: CREATE_STREAM, payload: response.data });
+  // we will force the user to go to a certain url after the success of the creation
+
+  history.push('/')
 };
 
 export const fetchStreams = () => async dispatch => {
