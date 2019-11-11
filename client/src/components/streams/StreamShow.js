ea13e3e34efe0cff98315dbe4088f4a791621778
Nour Adel <noura7305@gmail.com>
2019-11-11 16:33:15 +0200

    show a stream

--- client/src/components/streams/StreamShow.js
+++ client/src/components/streams/StreamShow.js
@@ -1,7 +1,28 @@
-import React from "react";
+import React, { Component } from "react";
+import { connect } from "react-redux";
 
-const StreamShow = () => {
-  return <div>StreamShow</div>;
+import { fetchStream } from "../../actions";
+
+class StreamShow extends Component {
+  componentDidMount() {
+    this.props.fetchStream(this.props.match.params.id);
+  }
+  render() {
+    if (!this.props.stream) {
+      return <div>loading</div>;
+    }
+    return (
+      <div>
+        <h1>{this.props.stream.title}</h1>
+        <h5>{this.props.stream.description}</h5>
+      </div>
+    );
+  }
+}
+const mapStateToProps = (state, ownProps) => {
+  return { stream: state.streams[ownProps.match.params.id] };
 };
-
-export default StreamShow;
+export default connect(
+  mapStateToProps,
+  { fetchStream }
+)(StreamShow);
